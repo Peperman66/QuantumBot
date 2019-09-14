293695432e3059a27e4cfcfb26e294f953f7f8c9
Unknown <peperman66@seznam.cz>
2019-09-14 21:04:07 +0200

    add activity
    Added game activity

--- index.js
+++ index.js
@@ -26,37 +26,10 @@ bot.registry
     // Registers all of your commands in the ./commands/ directory
     .registerCommandsIn(path.join(__dirname, config.commandDirectory));
 
-/*bot.commands = new Discord.Collection();
-
-fs.readdir(config.commandDirectory, (err, files) => {
-    if (err) return console.log(err);
-
-    let jsfile = files.filter(f => f.split('.').pop() === 'js');
-
-    if (jsfile.length <= 0) return console.log("Couldn't find commands.");
-
-    jsfile.forEach((f, i) => {
-        let props = require(config.commandDirectory + `/${f}`);
-        console.log(`${f} loaded`);
-        bot.commands.set(props.help.name, props);
-    });
-
+bot.on('ready', () => {
+    bot.user.setActivity(',help for help');
 });
 
-bot.on('message', async message => {
-    if (message.author.bot) return;
-    if (message.channel.type == 'dm') return;
-    let prefix = config.prefix;
-    let messageArray = message.content.split(' ');
-    let cmd = messageArray[0];
-    let args = messageArray.splice(1);
-    let commandFile = bot.commands.get(cmd.slice(prefix.length));
-    if (cmd.slice(0, config.prefix.length) === config.prefix) {
-        await commandFile.run(bot, message, args)
-            .catch((err) => console.log(err));
-    }
-});
-*/
 bot.on('error', async error => console.log(error));
 
 bot.login(process.env.TOKEN);
