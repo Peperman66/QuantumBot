25cd78bc4e408a6be30c74a6516b33bd860df7c4
Peperman66 <peperman66@seznam.cz>
2019-10-02 19:39:58 +0200

    add purge command
    Added purge command

---
+++ commands/util/purge.js
@@ -0,0 +1,26 @@
+const Commando = require("discord.js-commando");
+const Discord = require("discord.js");
+
+module.exports = class ReplyCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'purge',
+            group: 'util',
+            memberName: 'purge',
+            description: 'Removes messages from the channel',
+            examples: ['purge 10'],
+            args: [{
+                key: 'number of messages',
+                label: 'messages',
+                prompt: 'How many messages do you want to remove?',
+                type: 'integer'
+            }]
+        });
+    }
+
+    run(message, { messages }) {
+        message.channel.fetchMessages({ limit: (messages+1) })
+            .then(messages => message.channel.bulkDelete(messages));
+        
+    }
+}
